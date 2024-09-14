b03689c2a626e9def20cbaf856c496f04a6c24c9
gabiellima-wav <seuemail@example.com>
2024-09-13 22:01:30 -0300

    botoes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -44,6 +43,11 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+
+    // Retrofit e GSON
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/prospapp/AnaliseActivity.kt
+++ app/src/main/java/com/example/prospapp/AnaliseActivity.kt
@@ -1,12 +1,34 @@
 package com.example.prospapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class AnaliseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_analise)
+
+        // Configuração do BottomNavigationView
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_home -> {
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    true
+                }
+                R.id.action_analise -> {
+                    // A tela de análise já está em exibição, portanto, não precisa fazer nada
+                    true
+                }
+                R.id.action_agenda -> {
+                    startActivity(Intent(this, AgendaActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
