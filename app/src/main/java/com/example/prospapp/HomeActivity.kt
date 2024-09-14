40ac2a932edfe1950f25de7cc78e30c1bb38d203
gabiellima-wav <seuemail@example.com>
2024-09-13 22:01:30 -0300

    botoes

--- app/src/main/java/com/example/prospapp/HomeActivity.kt
+++ app/src/main/java/com/example/prospapp/HomeActivity.kt
@@ -12,22 +12,26 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        // Define o item "Home" como selecionado ao carregar a tela
+        bottomNavigationView.selectedItemId = R.id.action_home
+
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-                when (item.itemId) {
-            R.id.action_home -> {
-                // Home está na tela atual, não faz nada
-                true
-            }
-            R.id.action_analise -> {
-                startActivity(Intent(this, AnaliseActivity::class.java))
-                true
-            }
-            R.id.action_agenda -> {
-                startActivity(Intent(this, AgendaActivity::class.java))
-                true
-            }
+            when (item.itemId) {
+                R.id.action_home -> {
+                    // Home está na tela atual, não faz nada
+                    true
+                }
+                R.id.action_analise -> {
+                    startActivity(Intent(this, AnaliseActivity::class.java))
+                    true
+                }
+                R.id.action_agenda -> {
+                    startActivity(Intent(this, AgendaActivity::class.java))
+                    true
+                }
                 else -> false
-        }
+            }
         }
     }
 }
